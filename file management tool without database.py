5e0d4b6063913270a196ea1b94c3f813539365a4
Avinashervin <49512939+Avinashervin@users.noreply.github.com>
2019-05-08 11:01:41 +0530

    Add files via upload

---
+++ file management tool without database.py
@@ -0,0 +1,63 @@
+import tkinter
+from tkinter import *
+from tkinter import messagebox
+from tkinter import filedialog
+import os
+import shutil
+import mysql
+
+
+main_win = tkinter.Tk()
+main_win.title("File Management Application")
+message='Note :SELECT THE LOCAL SERVER BEFORE USING THE TOOL'
+w = Label(main_win, text=message)
+w.config(bg='lightgreen')
+w.pack()
+main_win.geometry("350x450")
+
+def chooseDir():
+    main_win.sourcedir =  filedialog.askdirectory(parent=main_win, initialdir= "/", title='Please select a directory')
+    x=input("Enter a folder name starting with \\\:")
+    shutil.copytree(main_win.sourcedir,main_win.changeser+x)
+
+
+def chooseFile():
+    main_win.sourcefile = filedialog.askopenfilename(parent=main_win, initialdir= "/", title='Please select a directory')
+    shutil.copy2(main_win.sourcefile,main_win.changeser)
+
+def Server():
+    os.startfile(main_win.changeser)
+
+def changeser():
+    main_win.changeser =  filedialog.askdirectory(parent=main_win, initialdir= "/", title='Select Server')
+
+def openfile():
+
+    f = []
+    f=os.listdir(main_win.changeser)
+    print(f)
+    n=int(input("Select the index of the file :"))
+    filename=input("Enter the new file name for the duplication of the source file :")
+    shutil.copy2(main_win.changeser+'/'+f[n],main_win.changeser+'/'+filename)
+    text = open(main_win.changeser+'/'+filename, "r+")
+    print(text.read())
+    to_add=input("Write the file : ")
+    text.write(to_add)
+    text.close()
+
+b_changeser = tkinter.Button(main_win, text = "Select the Local server", width = 30, height = 3, command = changeser)
+b_changeser.place(x = 65,y = 50)
+
+b_chooseDir = tkinter.Button(main_win, text = "Upload Entire Folder", width = 30, height = 3, command = chooseDir)
+b_chooseDir.place(x =65,y = 125)
+
+b_chooseFile = tkinter.Button(main_win, text = "Upload File", width = 30, height = 3, command = chooseFile)
+b_chooseFile.place(x = 65,y = 200)
+
+b_Server = tkinter.Button(main_win, text = "Open Local Server", width = 30, height = 3, command = Server)
+b_Server.place(x = 65,y = 275)
+
+b_Server = tkinter.Button(main_win, text = "Open a File", width = 30, height = 3, command = openfile)
+b_Server.place(x = 65,y = 350)
+
+main_win.mainloop()
